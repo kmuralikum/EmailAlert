4b361e8bc26913a7a6d3163db5f9d05f484c6651
kmuralikum <kmuralikum2002@gmail.com>
2024-01-30 13:10:29 +0530

    renamed

---
+++ Server/utils/DailyattendanceCron.js
@@ -0,0 +1,21 @@
+const {CronJob} = require("cron")
+const {attndEmail} = require("../services/DailyEmail")
+const Content = require("../model/ContentModel")
+const Alert = require("../model/alertModel")
+
+//0 30 8 * * 1-5 
+//running cron for sending daily attendance mail
+const cron = new CronJob("*/2 * * * *",async()=>{  
+    const content = await Content.find()
+    const alerts = await Alert.find()
+    let filteredContent = content.filter((c)=>c.content==='Mail of Daily attendance report').forEach(content=> {
+    const filteredAlert = alerts.filter(alert =>alert.events.includes(content.eventType))
+    
+        filteredAlert.forEach((alert)=>{
+            console.log("Daily",alert.email);
+            attndEmail(alert.email) 
+   })
+   })   
+}) 
+
+module.exports = cron 
